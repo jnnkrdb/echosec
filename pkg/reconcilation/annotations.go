91193fe6f9027bc8ee086b80e577ddb91c51e47f
jnnkrdb <jannek.rodenburger@icloud.com>
2025-04-11 22:28:53 +0000

    commented unused label constant

--- pkg/reconcilation/annotations.go
+++ pkg/reconcilation/annotations.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	LabelEnableMirror      string = "echosec.jnnkrdb.de/mirror-me"
+	//LabelEnableMirror      string = "echosec.jnnkrdb.de/mirror-me"
 	AnnotationRegexAvoid   string = "echosec.jnnkrdb.de/rgx.avoid"
 	AnnotationRegexMatch   string = "echosec.jnnkrdb.de/rgx.match"
 	AnnotationSourceObject string = "echosec.jnnkrdb.de/src.object"
